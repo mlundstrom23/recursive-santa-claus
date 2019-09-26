6e09b180ce16c8548928400f707d56660716d852
Marina Lundstrom <marina@marinas-air.worcester.local>
2019-09-25 21:32:37 -0400

    Test successfully recursively delivers presents 7 times

--- santa.js
+++ santa.js
@@ -1,11 +1,11 @@
 function deliverPresents(houses) {
   if(houses.length === 0) return 0
-  console.log('Delivering presents to ' + first)
-
+  
   var first = houses[0]
   var rest = houses.slice(1, houses.length)
-  
+  console.log('Delivering presents to ' + first)
   deliverPresents(rest)
+  
 }
 
 module.exports = deliverPresents
