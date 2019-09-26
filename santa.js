529c0e516bd7a8d035a00ef3f10715448b7b0696
Marina Lundstrom <marina@marinas-air.worcester.local>
2019-09-25 21:04:16 -0400

    Test now recurses the function 7 times

--- santa.js
+++ santa.js
@@ -1,5 +1,11 @@
 function deliverPresents(houses) {
+  if(houses.length === 0) return 0
   console.log('Delivering presents to ' + houses[0])
+
+  var first = houses[0]
+  var rest = houses.slice(1, houses.length)
+  
+  deliverPresents(rest)
 }
 
 module.exports = deliverPresents
